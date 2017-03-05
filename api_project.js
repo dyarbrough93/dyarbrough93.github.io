c2b08430220cc7711832715348985eae4827329e
Davis Yarbrough <dyarbrough@nosco.com>
2017-03-05 16:20:42 -0600

    apidocs

---
+++ api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "Core-Services API Documentation",
+  "version": "1.0.0",
+  "description": "Description of the API defined by the core-services project.",
+  "title": "Core-Services API",
+  "url": "https://app.domain.com",
+  "template": {
+    "withCompare": true,
+    "withGenerator": false
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-03-05T21:24:09.057Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
